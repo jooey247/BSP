76de2fc209c1d1b1b71a2fe15dfbf8e70f90e459
jeongdei <jeongdei@users.noreply.github.com>
2016-08-08 08:59:33 +0530

    toggle 버튼 초기화 content_main layout과 연결

--- BeaconSecurityApp2/app/src/main/java/cse/knu/beaconsecurityapp/SetActivity.java
+++ BeaconSecurityApp2/app/src/main/java/cse/knu/beaconsecurityapp/SetActivity.java
@@ -7,6 +7,7 @@ import android.media.AudioManager;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class SetActivity extends AppCompatActivity {
 
@@ -28,10 +29,15 @@ public class SetActivity extends AppCompatActivity {
     private int camera_info = 2;
     private int flash_info = 3;
 
+    ToggleButton toggle_Button;
+    ToggleButton toggle_Button2;
+    ToggleButton toggle_Button3;
+    ToggleButton toggle_Button4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_set);
+        setContentView(R.layout.content_main); //activity_set);
 
         camera = android.hardware.Camera.open();
         camera.release();
@@ -39,6 +45,13 @@ public class SetActivity extends AppCompatActivity {
         audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+
+        toggle_Button=(ToggleButton)findViewById(R.id.toggleButton);
+        toggle_Button2=(ToggleButton)findViewById(R.id.toggleButton2);
+        toggle_Button3=(ToggleButton)findViewById(R.id.toggleButton3);
+        toggle_Button4=(ToggleButton)findViewById(R.id.toggleButton4);
+
+        getUsersOption();
     }
 
     public SetActivity(){}
@@ -49,27 +62,33 @@ public class SetActivity extends AppCompatActivity {
             case 0 :
                 //wifi끄기
                 wifiManager.setWifiEnabled(false);
+                toggle_Button.setChecked(false);
                 break;
             case 1 :
                 wifiManager.setWifiEnabled(true);
+                toggle_Button.setChecked(true);
                 break;
             default:
         }
         switch (opt.indexOf(sound_info)){
             case 0 :
                 audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                toggle_Button2.setChecked(false);
                 break;
             case 1 :
                 audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                toggle_Button3.setChecked(true);
                 break;
             default:
         }
         switch (opt.indexOf(camera_info)){
             case 0 :
                 camera.lock();
+                toggle_Button3.setChecked(false);
                 break;
             case 1 :
                 camera.release();
+                toggle_Button4.setChecked(true);
                 break;
             default:
         }
@@ -89,12 +108,15 @@ public class SetActivity extends AppCompatActivity {
         switch (audioManager.getRingerMode()){
             case AudioManager.RINGER_MODE_NORMAL:
                 audiostate="1";
+                toggle_Button2.setChecked(true);
                 break;
             case AudioManager.RINGER_MODE_SILENT:
                 audiostate="0";
+                toggle_Button2.setChecked(false);
                 break;
             case AudioManager.RINGER_MODE_VIBRATE:
                 audiostate="0";
+                toggle_Button2.setChecked(false);
                 break;
             default:
         }
@@ -102,9 +124,11 @@ public class SetActivity extends AppCompatActivity {
         switch (wifiManager.getWifiState()){
             case WifiManager.WIFI_STATE_ENABLED:
                 wifistate="1";
+                toggle_Button.setChecked(true);
                 break;
             case WifiManager.WIFI_STATE_DISABLING:
                 wifistate="0";
+                toggle_Button.setChecked(false);
                 break;
             default:
         }
@@ -112,12 +136,16 @@ public class SetActivity extends AppCompatActivity {
         switch (p.getFlashMode()){
             case android.hardware.Camera.Parameters.FLASH_MODE_TORCH:
                 flashstate="1";
+                toggle_Button4.setChecked(true);
                 break;
             case android.hardware.Camera.Parameters.FLASH_MODE_OFF:
                 flashstate="0";
+                toggle_Button4.setChecked(false);
                 break;
             default:
         }
+
+        toggle_Button3.setChecked(true);
 
         user_original_option_info = wifistate+audiostate+camerastate+flashstate;
     }
